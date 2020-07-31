23df14592621551120a9e8ed80498668e38dc896
Richard Hale <RH1085@txstate.edu>
2020-07-30 20:22:03 -0500

    Returning Questions from the database

--- .aws-sam/build/ExamBotFunction/app.js
+++ .aws-sam/build/ExamBotFunction/app.js
@@ -61,57 +61,10 @@ const QuizHandler = {
         attributes.counter = 0;
         attributes.quizScore = 0;
 
-        // run SQL command
-        let data = await rdsDataService.executeStatement(sqlParamsGetQuestions, function (err, data) {
-            if (err) {
-                // error
-                console.log(err)
-                //callback('Query Failed')
-            } else {
-                // init
-                var rows = []
-                var cols = []
-
-                // build an array of columns
-                data.columnMetadata.map((v, i) => {
-                    cols.push(v.name)
-                });
-
-                // build an array of rows: { key=>value }
-                data.records.map((r) => {
-                    var row = {}
-                    r.map((v, i) => {
-                        if (v.stringValue !== "undefined") {
-                            row[cols[i]] = v.stringValue;
-                        } else if (v.blobValue !== "undefined") {
-                            row[cols[i]] = v.blobValue;
-                        } else if (v.doubleValue !== "undefined") {
-                            row[cols[i]] = v.doubleValue;
-                        } else if (v.longValue !== "undefined") {
-                            row[cols[i]] = v.longValue;
-                        } else if (v.booleanValue !== "undefined") {
-                            row[cols[i]] = v.booleanValue;
-                        } else if (v.isNull) {
-                            row[cols[i]] = null;
-                        }
-                    })
-                    rows.push(row)
-                })
-
-                // done
-                //console.log('Found rows: ' + rows.length)
-                //console.log(rows[1])
-                //console.log(rows[1].question)
-                //console.log(rows[1].question);
-                //question = rows[1].question;
-                //console.log(question);
-                //callback(null, rows)
-                return rows;
-            }
-        }).promise()
 
         //var question = askQuestion(handlerInput);
-        var question = data[1].question;
+        var results = await queryQuestions();
+        var question = results[1].question;
         console.log(question);
         var speakOutput = startQuizMessage + question;
         var repromptOutput = question;
@@ -438,6 +391,93 @@ function supportsDisplay(handlerInput) {
         handlerInput.requestEnvelope.context.System.device.supportedInterfaces &&
         handlerInput.requestEnvelope.context.System.device.supportedInterfaces.Display
     return hasDisplay;
+}
+/*
+async function queryQuestions(){
+    // run SQL command
+    let data = await rdsDataService.executeStatement(sqlParamsGetQuestions, function (err, data) {
+        if (err) {
+            // error
+            console.log(err)
+            //callback('Query Failed')
+        } else {
+            // init
+            var rows = []
+            var cols = []
+
+            // build an array of columns
+            data.columnMetadata.map((v, i) => {
+                cols.push(v.name)
+            });
+
+            // build an array of rows: { key=>value }
+            data.records.map((r) => {
+                var row = {}
+                r.map((v, i) => {
+                    if (v.stringValue !== "undefined") {
+                        row[cols[i]] = v.stringValue;
+                    } else if (v.blobValue !== "undefined") {
+                        row[cols[i]] = v.blobValue;
+                    } else if (v.doubleValue !== "undefined") {
+                        row[cols[i]] = v.doubleValue;
+                    } else if (v.longValue !== "undefined") {
+                        row[cols[i]] = v.longValue;
+                    } else if (v.booleanValue !== "undefined") {
+                        row[cols[i]] = v.booleanValue;
+                    } else if (v.isNull) {
+                        row[cols[i]] = null;
+                    }
+                })
+                rows.push(row)
+            })
+
+            // done
+            //console.log('Found rows: ' + rows.length)
+            //console.log(rows[1])
+            //console.log(rows[1].question)
+            //console.log(rows[1].question);
+            //question = rows[1].question;
+            //console.log(question);
+            //callback(null, rows)
+            return rows;
+        }
+    }).promise();
+}
+*/
+
+async function queryQuestions(){
+    // run SQL command
+    let data = await rdsDataService.executeStatement(sqlParamsGetQuestions).promise()
+
+    var rows = []
+    var cols = []
+
+    // build an array of columns
+    data.columnMetadata.map((v, i) => {
+        cols.push(v.name)
+    });
+
+    // build an array of rows: { key=>value }
+    data.records.map((r) => {
+        var row = {}
+        r.map((v, i) => {
+            if (v.stringValue !== "undefined") {
+                row[cols[i]] = v.stringValue;
+            } else if (v.blobValue !== "undefined") {
+                row[cols[i]] = v.blobValue;
+            } else if (v.doubleValue !== "undefined") {
+                row[cols[i]] = v.doubleValue;
+            } else if (v.longValue !== "undefined") {
+                row[cols[i]] = v.longValue;
+            } else if (v.booleanValue !== "undefined") {
+                row[cols[i]] = v.booleanValue;
+            } else if (v.isNull) {
+                row[cols[i]] = null;
+            }
+        })
+        rows.push(row)
+    })
+    return rows;
 }
 
 function getBadAnswer(item) {
